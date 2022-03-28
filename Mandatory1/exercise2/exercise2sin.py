ce37f332c2ef2e162c8465a2b75a4421d4e92ed3
Hegewald1 <eaamheg@students.eaaa.dk>
2022-03-28 16:18:52 +0200

    exercise2 almost finished

--- Mandatory1/exercise2/exercise2sin.py
+++ Mandatory1/exercise2/exercise2sin.py
@@ -1,31 +1,30 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, BatchNormalization, Flatten
 
-# select 5000 random floats from interval -2 to 6
-x = np.random.uniform(low=-2, high=6, size=(5000, 1))
-y = x*x*x - 6*x**2 + 4*x + 12
+# select 2500 random floats from interval 0 to 10
+x = np.random.uniform(low=0, high=10, size=(2500, 1))
+y = np.sin(x)
 
 model = Sequential()
-model.add(Dense(80, input_dim=1, activation='sigmoid'))
-model.add(Dense(40, activation='sigmoid'))
-model.add(Dense(20, activation='sigmoid'))
-model.add(Dense(10, activation='sigmoid'))
+model.add(Dense(100, input_dim=1, activation='sigmoid'))
+model.add(Dense(100, activation='sigmoid'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(x, y, epochs=150, batch_size=50)
-
-
-predictions = model.predict([-1.5, 0.8, 4.2, 5])
-print(predictions)  # Approximately -10.875, 11.872, -2.952, 7
+model.summary()
+# predictions = model.predict([-1.5, 0.8, 4.2, 5])
+# print(predictions)  # Approximately -, -, -, -
 
 y_new = model.predict(x)
 
 plt.subplot(2, 1, 1)
 plt.scatter(x, y, color='blue', s=1)
-plt.suptitle('y = $x^3$ - 6$x^2$ + 4x + 12')
+plt.suptitle('y = sin(x)')
 plt.ylabel('Real y')
 plt.grid()
 
